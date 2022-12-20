95e49f8bc4ef2f971636d80e68b39791457cc5a7
Anton Gadyatskiy <aoagadbart90@gmail.com>
2022-12-21 01:07:30 +0300

    сделал первую задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double degree (int a, int b) 
+{
+    double res = 1;
+    res = Math.Pow(a,b);
+    return res;
+}
+
+Console.WriteLine("Введите А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите B:");
+int b = Convert.ToInt32(Console.ReadLine());
+double result = degree(a,b);
+Console.WriteLine(result);
+
+
