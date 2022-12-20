c9c7b82da0c3fd42e3030146c3c38a7e1015ac52
Anton Gadyatskiy <aoagadbart90@gmail.com>
2022-12-21 01:13:13 +0300

    сделал вторую задачу

--- Program.cs
+++ Program.cs
@@ -3,18 +3,41 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-double degree (int a, int b) 
+// double degree (int a, int b) 
+// {
+//     double res = 1;
+//     res = Math.Pow(a,b);
+//     return res;
+// }
+
+// Console.WriteLine("Введите А:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите B:");
+// int b = Convert.ToInt32(Console.ReadLine());
+// double result = degree(a,b);
+// Console.WriteLine(result);
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int Sum (int num) 
 {
-    double res = 1;
-    res = Math.Pow(a,b);
-    return res;
+    int result = 0;
+    int diff = 0;
+    while (num>0)
+    {
+        diff = num % 10;
+        result += diff;
+        num = num / 10;
+    }
+    return result;
 }
 
-Console.WriteLine("Введите А:");
+Console.WriteLine("Введите число:");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите B:");
-int b = Convert.ToInt32(Console.ReadLine());
-double result = degree(a,b);
+int result = Sum(a);
 Console.WriteLine(result);
-
-
